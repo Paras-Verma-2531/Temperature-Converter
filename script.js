c7c84eccf21ebec89e0a1428ae01e27a8e93304f
paras <parasverma2531@gmail.com>
2023-06-29 23:46:06 +0530

    EventListner added for kelvin conversion

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-var celciusValue=document.getElementById('Celcius');
-var fehrenheitValue=document.getElementById('Fehrenhiet');
-var kelvinValue= document.getElementById('Kelvin');
+// get values:
+var celciusValue = document.getElementById("Celcius");
+var fehrenheitValue = document.getElementById("Fehrenhiet");
+var kelvinValue = document.getElementById("Kelvin");
 
-const clearValue=()=>{
-   celciusValue.value="";
-   fehrenheitValue.value="";
-   kelvinValue.value="";
+//  method to clear the values
+const clearValue = () => {
+  celciusValue.value = null;
+  fehrenheitValue.value = null;
+  kelvinValue.value = null;
+};
+//round number
+function roundnumber(number)
+{
+    return Math.round(number*100)/100;
 }
-
-const celciusToOther=()=>{
-    document.getElementById('Fehrenhiet').value=1.8*document.getElementById('Celcius').value + 32;
-    document.getElementById('Kelvin').value=1*document.getElementById('Celcius').value+273.15;
-}
+// input event Listner to convert celcius to other units:
+celciusValue.addEventListener("input", () => {
+    let cval=parseFloat(celciusValue.value);
+    let fval=1.8*cval + 32;
+    let kval=cval+ 273.15;
+    fehrenheitValue.value=roundnumber(fval);
+    kelvinValue.value=roundnumber(kval); 
+});
+// function to covert fehrenhiet to other units:
+fehrenheitValue.addEventListener('input',()=>
+{
+    let fval=parseFloat(fehrenheitValue.value);
+    let cval=(5/9)*(fval-32);
+    let kval=(5/9)*(fval-32)+273.15;
+    celciusValue.value=roundnumber(cval);
+    kelvinValue.value=roundnumber(kval);
+});
+kelvinValue.addEventListener('input',()=>
+{
+    let kval=parseFloat(kelvinValue.value);
+    let fval=(kval-273.15) * 9/5 + 32;
+    let cval=kval-273.15;
+    celciusValue.value=roundnumber(cval);
+    fehrenheitValue.value=roundnumber(fval);
+})
